da0379bac92edd3038c86f2b23d03c312711869d
TNORTHERN <traynorthern@yahoo.com>
2020-05-05 21:08:43 -0500

    removing unneccesary files adding deletes

--- graphql/mutations/index.js
+++ graphql/mutations/index.js
@@ -1,12 +1,13 @@
 const { GraphQLObjectType } = require('graphql');
-const { addTechnology } = require('./technologymutations');
+const { addTechnology , deleteTechnology} = require('./technologymutations');
 const { addProject, deleteProject } = require('./projectmutations');
 const RootMutation = new GraphQLObjectType({
   name: "Mutations",
   fields: {
     addTechnology,
     addProject,
-    deleteProject
+    deleteProject,
+    deleteTechnology
   },
 });
 
